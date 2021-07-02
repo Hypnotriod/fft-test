83c50951ffabe450f296dc1948e6e593604d0565
ipikin <illia.pikin@dgngames.com>
2021-07-02 16:44:57 +0300

    Playing around

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 
 #include "FFT.h"
 
-#define FR_TABLE_HEIGHT 20
+#define SIGNAL_TABLE_HEIGHT 20
+#define POW_TABLE_HEIGHT 20
 #define TAPS_NUM 256
 
-void determineBaseFrequency(float * Pow, int N) {
+void printBaseFrequency(float * Pow, int N) {
     float frequency = 0.f;
     float fm = 0;
     float vm = 0;
@@ -65,29 +66,12 @@ void fillPow(float * Re, float * Im, float * Pow, int N) {
     }
 }
 
-void test() {
-    static float Re[TAPS_NUM];
-    static float Im[TAPS_NUM];
-    static float Pow[TAPS_NUM];
+void printPow(float * Pow, int N) {
     int i;
     int j;
-
-    generateSine(Re, Im, TAPS_NUM, 10.f, 0.7f);
-    generateSquare(Re, Im, TAPS_NUM, 35.45f, 0.8f);
-    generateSine(Re, Im, TAPS_NUM, 78.f, 0.5f);
-
-    FFT(Re, Im, TAPS_NUM, log2(TAPS_NUM), FT_DIRECT);
-
-    fillPow(Re, Im, Pow, TAPS_NUM);
-
-    printf("%10s | %10s | %10s\n", "Real", "Imaginary", "Power");
-    for (i = 0; i < TAPS_NUM; i++) {
-        printf("%10.6f | %10.6f | %10.6f\n", Re[i], Im[i], Pow[i]);
-    }
-
-    for (j = FR_TABLE_HEIGHT - 1; j >= 0; j--) {
-        for (i = 1; i < TAPS_NUM / 2 + 1; i++) {
-            if (Pow[i] * (float) FR_TABLE_HEIGHT >= j) {
+    for (j = POW_TABLE_HEIGHT - 1; j >= 0; j--) {
+        for (i = 1; i < N; i++) {
+            if (Pow[i] * (float) POW_TABLE_HEIGHT >= j) {
                 printf("#");
             } else {
                 printf(" ");
@@ -95,8 +79,78 @@ void test() {
         }
         printf("\n");
     }
+}
 
-    determineBaseFrequency(Pow, (TAPS_NUM / 2));
+void printSignal(float * Re, int N) {
+    int i;
+    int j;
+    for (j = SIGNAL_TABLE_HEIGHT - 1; j >= 0; j--) {
+        for (i = 0; i < N; i++) {
+            if (j > SIGNAL_TABLE_HEIGHT / 2) {
+                if (Re[i] * (float) (SIGNAL_TABLE_HEIGHT / 2) >= j - (SIGNAL_TABLE_HEIGHT / 2)) {
+                    printf("#");
+                } else {
+                    printf(" ");
+                }
+            } else {
+                if (Re[i] * (float) (SIGNAL_TABLE_HEIGHT / 2) <= j - (SIGNAL_TABLE_HEIGHT / 2)) {
+                    printf("#");
+                } else {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+void printCoefficients(float * Re, float * Im, float * Pow, int N) {
+    printf("%10s | %10s | %10s\n", "Real", "Imaginary", "Power");
+    int i;
+    for (i = 0; i < N; i++) {
+        printf("%10.6f | %10.6f | %10.6f\n", Re[i], Im[i], Pow[i]);
+    }
+}
+
+void printDivider(int N) {
+    int i;
+    for (i = 0; i < N; i++) {
+        printf(" ");
+    }
+    printf("\n");
+    for (i = 0; i < N; i++) {
+        printf("_");
+    }
+    printf("\n");
+    for (i = 0; i < N; i++) {
+        printf(" ");
+    }
+    printf("\n");
+}
+
+void test() {
+    static float Re[TAPS_NUM];
+    static float Im[TAPS_NUM];
+    static float Pow[TAPS_NUM];
+
+    generateSine(Re, Im, TAPS_NUM, 2.f, 0.7f);
+    generateSquare(Re, Im, TAPS_NUM, 35.75f, 0.8f);
+    generateSine(Re, Im, TAPS_NUM, 78.f, 0.5f);
+
+    printSignal(Re, TAPS_NUM / 2);
+    printDivider(TAPS_NUM / 2);
+
+    FFT(Re, Im, TAPS_NUM, log2(TAPS_NUM), FT_DIRECT);
+
+    fillPow(Re, Im, Pow, TAPS_NUM);
+
+
+    //    printCoefficients(Re, Im, Pow, TAPS_NUM);
+
+    printPow(Pow, TAPS_NUM / 2 + 1);
+    printDivider(TAPS_NUM / 2);
+
+    printBaseFrequency(Pow, (TAPS_NUM / 2));
 
     fflush(stdout);
 }
